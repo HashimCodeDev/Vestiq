884636058531879df9a5f9fc09927dea53564fea
Hashim <hashimmohammed212@gmail.com>
2025-05-30 12:44:52 +0530

    font changed to jakarta

--- frontend/src/app/components/WelcomeCard.tsx
+++ frontend/src/app/components/WelcomeCard.tsx
@@ -2,7 +2,7 @@ export default function WelcomeCard() {
   return (
     <>
       <div className="block max-w-md h-36 p-4 mask-origin-border bg-[#C78E58] rounded-3xl shadow-sm">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight dark:text-gray-900 text-white font-mono">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight dark:text-gray-900 text-white font-jakarta">
           Hi ✋ Olivia Bennett,
           <br />
           Welcome!{' '}
