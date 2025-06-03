c5eda858b4a9108ca2b5f601fa938c7072a634b7
Hashim <hashimmohammed212@gmail.com>
2025-06-03 23:19:08 +0530

    feat: update WardrobeSection to use structured carousel items and add navigation button

--- frontend/src/components/WardrobeSection.tsx
+++ frontend/src/components/WardrobeSection.tsx
@@ -5,14 +5,35 @@ import {
   CarouselItem,
 } from '@/components/ui/carousel';
 import Image from 'next/image';
+import { title } from 'process';
+import { useEffect } from 'react';
+import { Button } from './ui/button';
+import { ArrowRightIcon } from '@phosphor-icons/react';
+import { useRouter } from 'next/navigation';
 
 export default function WardrobeSection() {
+  const router = useRouter();
   const carouselItems = [
-    'https://i.pinimg.com/736x/92/f2/6e/92f26ef8411fdfb284e2b0fa773ee13e.jpg',
-    'https://i.pinimg.com/736x/86/b2/ba/86b2ba24874a08a00ac0b01a518bd56b.jpg',
-    'https://i.pinimg.com/736x/cf/64/ff/cf64ff0d45268609266ed17244f53c80.jpg',
-    'https://i.pinimg.com/736x/85/98/69/859869086499cde08c6633d1d26b5a42.jpg',
-    'https://i.pinimg.com/736x/09/de/98/09de9831f16476cafa75c3cc9ea5047b.jpg',
+    {
+      src: 'https://i.pinimg.com/736x/92/f2/6e/92f26ef8411fdfb284e2b0fa773ee13e.jpg',
+      title: 'Wardrobe item 1',
+    },
+    {
+      src: 'https://i.pinimg.com/736x/86/b2/ba/86b2ba24874a08a00ac0b01a518bd56b.jpg',
+      title: 'Wardrobe item 2',
+    },
+    {
+      src: 'https://i.pinimg.com/736x/cf/64/ff/cf64ff0d45268609266ed17244f53c80.jpg',
+      title: 'Wardrobe item 3',
+    },
+    {
+      src: 'https://i.pinimg.com/736x/85/98/69/859869086499cde08c6633d1d26b5a42.jpg',
+      title: 'Wardrobe item 4',
+    },
+    {
+      src: 'https://i.pinimg.com/736x/09/de/98/09de9831f16476cafa75c3cc9ea5047b.jpg',
+      title: 'Wardrobe item 5',
+    },
   ];
 
   return (
@@ -25,22 +46,35 @@ export default function WardrobeSection() {
               <div className="p-1">
                 <Card>
                   <CardContent className="flex aspect-square items-center justify-center">
-                    <span className="text-2xl font-semibold">
-                      <Image
-                        src={item}
-                        width={200}
-                        height={400}
-                        alt={`Wardrobe item ${index + 1}`}
-                        className="w-full h-full object-cover rounded-lg"
-                      />
-                    </span>
+                    <Image
+                      src={item.src}
+                      width={200}
+                      height={400}
+                      alt={`Wardrobe item ${index + 1}`}
+                      className="w-full h-full object-cover rounded-lg"
+                    />
                   </CardContent>
                 </Card>
+                <div className="mt-2 text-center font-medium text-black dark:text-white font-jakarta font-thin">
+                  {item.title}
+                </div>
               </div>
             </CarouselItem>
           ))}
         </CarouselContent>
       </Carousel>
+
+      <div className="flex justify-center">
+        <Button
+          className="mt-4 bg-gray-200 text-black dark:bg-black dark:text-white"
+          onClick={() => {
+            router.push('/wardrobe');
+          }}
+        >
+          <ArrowRightIcon size={16} />
+          Explore my Wardrobe
+        </Button>
+      </div>
     </div>
   );
 }
