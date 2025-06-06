a1cdef195b36127fdad118be71a4bd23e41b4c4d
Hashim <hashimmohammed212@gmail.com>
2025-06-07 02:05:06 +0530

    feat(WardrobeGrid): implement infinite scroll for wardrobe items and refactor state management

--- frontend/src/app/(main)/wardrobe/page.tsx
+++ frontend/src/app/(main)/wardrobe/page.tsx
@@ -1,9 +1,8 @@
 'use client';
 
-import { Suspense, useEffect, useState, lazy } from 'react';
+import { Suspense, useEffect, useState, lazy, useRef } from 'react';
 import ProtectedRoute from '@/components/ProtectedRoute';
 import WardrobePageSkeleton from '@/components/skeleton/WardrobePageSkeleton';
-import { Button } from '@/components/ui/button';
 import axios from '@/lib/axios';
 import { useAuth } from '@/context/AuthContext';
 
@@ -18,49 +17,12 @@ const WardrobeGrid = lazy(() => import('@/components/WardrobeGrid'));
  * - Uses `ProtectedRoute` to ensure authentication.
  */
 
-interface WardrobeItem {
-  _id: string;
-  userId: string;
-  imageUrl: string;
-  createdAt: string;
-  __v: number;
-}
-
 export default function Wardrobe() {
-  const [outfitItems, setOutfitItems] = useState<string[]>([]);
-  const { token } = useAuth();
-
-  useEffect(() => {
-    const fetchWardrobeItems = async () => {
-      if (!token) return;
-      try {
-        const response = await axios.get('/wardrobe', {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        const data = response.data as WardrobeItem[];
-        setOutfitItems(data.map((item) => item.imageUrl));
-      } catch (error) {
-        console.error('Error fetching wardrobe items:', error);
-      }
-    };
-    fetchWardrobeItems();
-  }, [token]);
-
-  useEffect(() => {
-    console.log('outfitItems:', outfitItems);
-  }, [outfitItems]);
-
   return (
     <ProtectedRoute>
       <div>
         <Suspense fallback={<WardrobePageSkeleton />}>
-          <div className="fixed right-0 bottom-0 z-40">
-            <Button>Add Outfit</Button>
-          </div>
-          <WardrobeGrid outfitItems={outfitItems} />
+          <WardrobeGrid />
         </Suspense>
       </div>
     </ProtectedRoute>
--- frontend/src/components/WardrobeGrid.tsx
+++ frontend/src/components/WardrobeGrid.tsx
@@ -9,7 +9,7 @@ import Link from 'next/link';
 import { Button } from './ui/button';
 import { PlusCircleIcon, PlusIcon } from '@phosphor-icons/react';
 import { useUploadImage } from '@/hooks/useUploadImage';
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Input } from './ui/input';
 import axios from '@/lib/axios';
 import { useAuth } from '@/context/AuthContext';
@@ -21,18 +21,110 @@ import { useAuth } from '@/context/AuthContext';
  * first card is a button to add a new outfit item. The component also includes
  * a filter badge to filter the wardrobe items based on different criteria.
  *
- * @param outfitItems List of URLs of the images of the outfit items to display
+ * @param wardrobeItems List of URLs of the images of the outfit items to display
  * @returns A JSX element representing the Wardrobe grid
  */
-export default function WardrobeGrid({
-  outfitItems,
-}: {
-  outfitItems: string[];
-}) {
+
+interface WardrobeItem {
+  _id: string;
+  userId: string;
+  imageUrl: string;
+  createdAt: string;
+  __v: number;
+}
+
+export default function WardrobeGrid() {
   const fileInputRef = useRef<HTMLInputElement>(null);
   const { uploadImage, isUploading } = useUploadImage();
   const { token } = useAuth();
+  const [wardrobeItems, setWardrobeItems] = useState<string[]>([]);
+  const [limit] = useState(5);
+  const [skip, setSkip] = useState(0);
+  const [hasMore, setHasMore] = useState(true);
+  const loaderRef = useRef<HTMLDivElement | null>(null);
+  const hasFetched = useRef(false);
 
+  /**
+   * Fetches the next batch of outfit items from the server.
+   * @param {number} [limit=5] The number of items to fetch.
+   * @param {number} [skip=0] The number of items to skip.
+   * @returns {Promise<void>} A promise that resolves when the items are fetched.
+   */
+  const fetchWardrobeItems = async (
+    limit: number = 5,
+    skip: number = 0,
+  ): Promise<void> => {
+    try {
+      const response = await axios.get(
+        `/wardrobe?limit=${limit}&skip=${skip}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+
+      const newItems = response.data.items.map(
+        (item: WardrobeItem) => item.imageUrl,
+      );
+      const totalCount = response.data.totalCount;
+
+      /** Append new items to existing */
+      setWardrobeItems((prev) => [...prev, ...newItems]);
+
+      /** Check if there are more items */
+      setHasMore(skip + newItems.length < totalCount);
+    } catch (error) {
+      console.error('Error fetching outfit items:', error);
+    }
+  };
+
+  /**
+   * Loads more wardrobe items.
+   *
+   * If there are more items (i.e. `hasMore` is true), fetches the next batch of
+   * items by calling `fetchWardrobeItems` with the current `limit` and `skip`
+   * values. Then, increments `skip` by `limit` to prepare for the next fetch.
+   */
+  const handleLoadMore = async () => {
+    if (hasMore) {
+      await fetchWardrobeItems(limit, skip);
+      setSkip((prevSkip) => prevSkip + limit);
+    }
+  };
+
+  // Fetch wardrobe items on mount
+  useEffect(() => {
+    if (!token || hasFetched.current) return;
+    fetchWardrobeItems(limit, skip);
+    hasFetched.current = true;
+    setSkip(limit);
+  }, [token, fetchWardrobeItems, limit, skip]);
+
+  useEffect(() => {
+    if (!loaderRef.current) return;
+    console.log('Infinite Scroll loading');
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          handleLoadMore();
+        }
+      },
+      { threshold: 1.0 },
+    );
+
+    observer.observe(loaderRef.current);
+
+    return () => observer.disconnect();
+  }, [handleLoadMore, hasMore]);
+
+  /**
+   * Simulates a click event on the file input element.
+   *
+   * When the user clicks the "Add Outfit" button, this function is called to
+   * open the file input dialog to select an image file to upload.
+   */
   const handleButtonClick = () => {
     fileInputRef.current?.click();
   };
@@ -59,8 +151,6 @@ export default function WardrobeGrid({
           },
         },
       );
-
-      console.log(res.data);
     } catch (err) {
       console.error('Error uploading image:', err);
     }
@@ -88,7 +178,7 @@ export default function WardrobeGrid({
         </Button>
       </div>
       <div className="grid grid-cols-2 gap-4 py-5 mb-10">
-        {outfitItems.map((item, index) => (
+        {wardrobeItems.map((item, index) => (
           <div key={index} className="p-1">
             <Link href={`/wardrobe/${index + 1}`}>
               <Card className="h-60 justify-center mb-5">
@@ -105,7 +195,7 @@ export default function WardrobeGrid({
             </Link>
           </div>
         ))}
-        {outfitItems.length > 5 && (
+        {wardrobeItems.length > 5 && (
           <div className="p-1">
             <Card className="h-60 bg-card flex justify-center items-center">
               <CardContent className="flex aspect-square items-center justify-center">
