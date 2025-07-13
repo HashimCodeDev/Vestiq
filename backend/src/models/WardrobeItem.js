d56d12c1c095a2724564cfeaeab9bffa1c5dd5af
Hashim <hashimmohammed212@gmail.com>
2025-07-13 16:24:06 +0530

    feat: integrate feature extraction for wardrobe items and update package manager version

--- backend/src/models/WardrobeItem.js
+++ backend/src/models/WardrobeItem.js
@@ -1,5 +1,25 @@
 import mongoose from "mongoose";
 
+const FeaturesSchema = new mongoose.Schema(
+	{
+		image_id: { type: String, required: true },
+		class_type: {
+			type: String,
+			enum: ["upper", "lower", "full"],
+			required: true,
+		},
+		type: { type: String, required: true },
+		color: { type: String, required: true },
+		texture: { type: String, required: true },
+		collar: { type: String, required: true },
+		sleeves: { type: String, required: true },
+		pattern: { type: String, required: true },
+		fit: { type: String, required: true },
+		size: { type: String, default: null },
+	},
+	{ _id: false }
+); // Disable _id for subdocument
+
 const WardrobeItemSchema = new mongoose.Schema({
 	userId: {
 		type: String,
@@ -14,6 +34,9 @@ const WardrobeItemSchema = new mongoose.Schema({
 		type: String,
 		required: true,
 	},
+
+	features: FeaturesSchema,
+
 	description: String,
 	createdAt: {
 		type: Date,
